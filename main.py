bd3a7c229fc702c0671c3d16741d5de1711017cb
Miguel Bendito <miguel.bendito02@gmail.com>
2025-12-11 19:07:13 -0500

    Update main.py
    replaced hardcoded pws with env vars

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = FLASK_SECRET_KEY
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -503,7 +503,7 @@ def send_email(name, email, phone, message, number_of_people, event_date, ocassi
     msg.attach(part2)
 
     EMAIL_KEY="miguel.bendito02@gmail.com"
-    PASSWORD_KEY="nwaq evwp bcbt eqwz"
+    PASSWORD_KEY= EMAIL_SECRET_KEY
 
     connection = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
     connection.starttls()
